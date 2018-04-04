0155958cc17c64ecc036ddfaaad68a892263356d
anuj1692 <38078283+anuj1692@users.noreply.github.com>
2018-04-04 20:56:57 +0530

    Add files via upload

---
+++ employee.module.js
@@ -0,0 +1,6 @@
+﻿(function () {
+
+    angular.module('app.core', [
+        'app.employee'
+    ]);
+})();
---
+++ employeeService.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    angular
+    .module('app.employee')
+    .service('employeeService', employeeService);
+    employeeService.$inject = ['$http'];
+    function employeeService($http) {
+        this.loadEmployee = loadEmployee;
+        loadEmployee();
+        function loadEmployee()
+        {   
+            return $http.get('http://localhost:55417/api/EmployeeTbl');
+        }
+    }
+}());
